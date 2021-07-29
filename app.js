4f46dd4c90e057524976ca6d7a8eca3eb711340f
Mohammad Alzamami <alzamami981@gmail.com>
2021-07-29 17:11:15 +0300

    add a db

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
+const db = require("./db/models");
 
-// app.listen(8000, () => {
-//   console.log("The application is running on localhost:8000");
-// });
+app.use(cors());
+
+const run = async () => {
+  try {
+    await db.sequelize.authenticate();
+    console.log("Connection to the database successful!");
+    await app.listen(8000, () => {
+      console.log("The application is running on localhost:8000");
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error);
+  }
+};
+
+run();
