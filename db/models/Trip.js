dbbfd99415ea7ac035fb2e9e35ed4cb5460eadc5
mohalenezi <ot.m.alenezi@gmail.com>
2021-07-30 17:18:15 +0300

    adding routes and controllers of trip

---
+++ db/models/Trip.js
@@ -0,0 +1,12 @@
+const SequelizeSlugify = require("sequelize-slugify");
+const { Sequelize } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  const Trip = sequelize.define("Trip", {
+    title: { type: DataTypes.STRING, allowNull: false },
+    description: { type: DataTypes.STRING, allowNull: false },
+    image: { type: DataTypes.STRING },
+  });
+  SequelizeSlugify.slugifyModel(Trip, { source: ["title"] });
+  return Trip;
+};
